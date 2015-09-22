fc51f8573a0d40f33b3449f898d7479fff45694d
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2015-09-21 21:11:04 -0300

    Update Image.h

--- Image/Project2/Image.h
+++ Image/Project2/Image.h
@@ -18,61 +18,62 @@ public:
 	int getPixel(int x, int y) {
 		return pixels[x + y*width];
 	}
-	void setARGB(int x, int y, int argb) {
-		pixels[x + y * width] = argb;
-	}
-	int getARGB(int x, int y) {
-		return pixels[x + y * width];
-	}
 	int getWidth() { return width; }
 	int getHeight() { return height; }
 
 	int* getPixels() {
 		return pixels;
 	}
-//	void setPixels(int* pixels) { //verificar 
-//		for (int i = 0; i < height; i++) {
-//			for (int j = 0; i < width; i++) {
-//				pixels[i]->setRGB()
-//			}
-//		}
-//	}
-	void dropPixels(){//da uma zerada nos pixels
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < width; j++) {
-					setPixel(0, i, j);
-				}
-			}
-	}
-	void plot(int* foreground, int xi, int yi, int largura, int altura) {
-		for (int i = 0; i < largura; i++) {
-			for (int j = 0; j < altura; j++) {
-				int a = (foreground[j] >> 24) & 0xff;
-				if (a == 0) {
-					continue;
-				}
-				else if (a = 255) {
-					setARGB(i, j, foreground[j]);
-				}
-				else {
-					int r = (foreground[i] >> 16) & 0xff;
-					int g = (foreground[i] >> 8) & 0xff;
-					int b = (foreground[i]) & 0xff;
-
-					int aThis = (getARGB(i, j) >> 24) & 0xff;
-					int rThis = (getARGB(i, j) >> 16) & 0xff;
-					int gThis = (getARGB(i, j) >> 8) & 0xff;
-					int bThis = (getARGB(i, j)) & 0xff;
-
-					int calcr = (rThis*(1-a)+r*a);
-					int calcg = (gThis*(1 - a) + r*a);
-					int calcb = (bThis*(1 - a) + r*a);
-					int argb = (255 << 24) | (calcr << 16) | (calcg << 8) | (calcb);
-					setARGB(i, j, argb);
-				}
+	//	void setPixels(int* pixels) { //verificar 
+	//		for (int i = 0; i < height; i++) {
+	//			for (int j = 0; i < width; i++) {
+	//				pixels[i]->setRGB()
+	//			}
+	//		}
+	//	}
+	void dropPixels() {//da uma zerada nos pixels
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				setPixel(0, i, j);
 			}
 		}
 	}
+	void plot(Image *foreground, int xi, int yi) { //verificar com o professor
+		for (int i = 0; i < foreground->getWidth() * foreground->getHeight(); i++) {
+			int x = i % foreground->getWidth();
+			int y = i / foreground->getWidth();
+			int argb = foreground->getPixel(x, y);
+			int x =+ xi;
+			int y = +yi;
+
+			int a = (argb >> 24) & 0xff;
+
+			if (a == 0) {
+				continue;
+			} 
+			else if (a == 255) {
+				this->setPixel(argb, x, y);
+			}
+			else {
+				int r = ((foreground->getPixel(x, y))>> 16) & 0xff;
+				int g = ((foreground->getPixel(x, y)) >> 8) & 0xff;
+				int b = (foreground->getPixel(x, y)) & 0xff;
+
+				int aThis = ((this->getPixel(x, y)) >> 24) & 0xff;
+				int rThis = ((this->getPixel(x, y)) >> 16) & 0xff;
+				int gThis = ((this->getPixel(x, y)) >> 8) & 0xff;
+				int bThis = (this->getPixel(x, y)) & 0xff;
+
+				int calcr = (rThis*(1 - a) + r*a);
+				int calcg = (gThis*(1 - a) + r*a);
+				int calcb = (bThis*(1 - a) + r*a);
+				int argb = (255 << 24) | (calcr << 16) | (calcg << 8) | (calcb);
+				foreground->setPixel(x, y, argb); //verificar com o professor
+			}
+		}
+	}
+
+//fazer sprite
 
 private:
 	int *pixels; // alternativamente char *pixels – 1 byte por canal
